91a89deec3e2aa3b4be345efd131495ba8255bd2
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2021-04-06 11:54:22 -0500

    add two components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-   
-    <HelloWorld msg="Testing echarts in vue App"/>
+    <div class="row"> 
+      <div class="col heightofAll"> 
+        <single-bar/>
+      </div>
+      <div class="col heightofAll">
+          <multi-bar/>
+      </div>
+    </div>
+        <div class="row"> 
+      <div class="col heightofAll"> 
+        <single-bar/>
+      </div>
+      <div class="col heightofAll">
+          <multi-bar/>
+      </div>
+    </div>
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import SingleBar from './components/Bar/SingleBar.vue';
+import MultiBar from './components/Bar/MultiBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SingleBar,
+    MultiBar
+
   }
 }
 </script>
@@ -24,5 +43,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+.heightofAll{
+  height: 36vh;
 }
 </style>
---
+++ src/components/Bar/SingleBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>single bar</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data:()=>{
+        return ({
+        })
+    },
+    props:{
+
+    },
+    methods:{
+     
+    }
+}
+</script>
+
+<style>
+
+</style>
